70a4f4eb42b1d802214a85c80ea9f20870459d67
JohnJim0816 <johnjim0816@gmail.com>
2020-05-23 09:35:41 +0800

    add 颜色分类

---
+++ 数组/75.颜色分类.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+@Author: John
+@Email: johnjim0816@gmail.com
+@Date: 2020-05-23 08:58:07
+@LastEditor: John
+@LastEditTime: 2020-05-23 09:10:20
+@Discription: 
+@Environment: python 3.7.7
+'''
+#
+# @lc app=leetcode.cn id=75 lang=python3
+#
+# [75] 颜色分类
+#
+
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        '''
+        荷兰三色旗问题解
+        '''
+        # 对于所有 idx < p0 : nums[idx < p0] = 0
+        # curr是当前考虑元素的下标
+        p0 = curr = 0
+        # 对于所有 idx > p2 : nums[idx > p2] = 2
+        p2 = len(nums) - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            else:
+                curr += 1
+# @lc code=end
+

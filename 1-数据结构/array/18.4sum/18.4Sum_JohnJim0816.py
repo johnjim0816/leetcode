c9019d347158eff42879b0a04fea5905d5e32c7e
JohnJim0816 <johnjim0816@gmail.com>
2020-08-02 14:44:50 +0800

    add 18.4sum

--- 1-数据结构/array/18.4sum/18.4Sum_JohnJim0816.py
+++ 1-数据结构/array/18.4sum/18.4Sum_JohnJim0816.py
@@ -3,20 +3,42 @@
 '''
 @Author: John
 @Email: johnjim0816@gmail.com
-@Date: 2020-05-11 17:56:24
+@Date: 2020-08-02 14:43:34
 @LastEditor: John
-@LastEditTime: 2020-05-21 16:45:36
+@LastEditTime: 2020-08-02 14:44:08
 @Discription: 
-@Environment: python 3.7.7
+@Environment: 
 '''
-#
-# @lc app=leetcode.cn id=18 lang=python3
-#
-# [18] 四数之和
-#
+# Source : https://leetcode.com/problems/4sum/
+# Author : JohnJim0816
+# Date   : 2020-08-02
 
-# @lc code=start
+##################################################################################################### 
+#
+# Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums 
+# such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of 
+# target.
+# 
+# Note:
+# 
+# The solution set must not contain duplicate quadruplets.
+# 
+# Example:
+# 
+# Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+# 
+# A solution set is:
+# [
+#   [-1,  0, 0, 1],
+#   [-2, -1, 1, 2],
+#   [-2,  0, 0, 2]
+# ]
+# 
+#####################################################################################################
+
 class Solution:
+    '''在三数之和排序+双指针的基础上，再加一层for循环。
+    '''
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         if not nums or len(nums) < 4:
             return []
@@ -45,5 +67,3 @@ class Solution:
                     else:
                         d -= 1
         return res
-# @lc code=end
-

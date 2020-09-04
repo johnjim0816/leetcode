5279e3c6023dc8fecea5247d26ac848a67ad8eb5
JohnJim0816 <johnjim0816@gmail.com>
2020-09-05 06:17:15 +0800

    add 143.reorder_list

--- basic/linked_list/143.reorder_list/143.ReorderList_JohnJim0816.py
+++ basic/linked_list/143.reorder_list/143.ReorderList_JohnJim0816.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
 # coding=utf-8
 '''
-@Author: John
-@Email: johnjim0816@gmail.com
-@Date: 2020-07-05 08:57:41
-@LastEditor: John
-@LastEditTime: 2020-07-05 09:53:12
-@Discription: 
-@Environment: python 3.7.7
+Author: John
+Email: johnjim0816@gmail.com
+Date: 2020-09-05 06:16:22
+LastEditor: John
+LastEditTime: 2020-09-05 06:16:48
+Discription: 
+Environment: 
 '''
-#
-# @lc app=leetcode.cn id=143 lang=python3
-#
-# [143] 重排链表
-#
+# Source : https://leetcode.com/problems/reorder-list/
+# Author : JohnJim0816
+# Date   : 2020-09-05
 
-# @lc code=start
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+##################################################################################################### 
+#
+# Given a singly linked list L: L0&rarr;L1&rarr;&hellip;&rarr;Ln-1&rarr;Ln,
+# reorder it to: L0&rarr;Ln&rarr;L1&rarr;Ln-1&rarr;L2&rarr;Ln-2&rarr;&hellip;
+# 
+# You may not modify the values in the list's nodes, only nodes itself may be changed.
+# 
+# Example 1:
+# 
+# Given 1->2->3->4, reorder it to 1->4->2->3.
+# 
+# Example 2:
+# 
+# Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+# 
+#####################################################################################################
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         '''双指针(pre,cur)迭代
@@ -55,7 +64,3 @@ class Solution:
         while p2:
             # 拼接
             p1.next, p2.next, p1, p2 = p2, p1.next, p1.next, p2.next
-        
-        
-# @lc code=end
-
